3323f8308f78593c0e410ab3daaf66c920c4ede1
than5466 <than5466@student.su.se>
2019-12-08 15:14:31 +0100

    :)

--- day_8/main.py
+++ day_8/main.py
@@ -5,21 +5,18 @@ def Split_To_Layers(Data, width, height):
     return [Data[index:index+step] for index in range(0,len(Data),step)]
 
 def Least_Zeroes(Layers):
-    
     Layer = 0
     number = float('inf')
-
     for layer in Layers:
         if layer.count('0') < number:
             number = layer.count('0')
             Layer = layer
-    
     return Layer
 
 def Program(Data, width, height):
     Layers = Split_To_Layers(Data,width,height)
-    Fewest_Zeroes = Least_Zeroes(Layers)
-    ans = Fewest_Zeroes.count('1')*Fewest_Zeroes.count('2')
+    Fewest_Zero_Layer = Least_Zeroes(Layers)
+    ans = Fewest_Zero_Layer.count('1')*Fewest_Zero_Layer.count('2')
     print("Answer, Part 1: {}".format(ans))
 
     image = ['2']*width*height
@@ -31,18 +28,12 @@ def Program(Data, width, height):
                     image[index] = ' '
                 elif layer[index] == '1':
                     image[index] = '█'
-    
     image = "".join(image)
-
     print("Answer, Part 2:")
     for i in range(height):
         print("{}".format(image[i*width:(i+1)*width]))
-    
-
-
 
 if __name__ == '__main__':
     f = open("data.txt", "r")
     In_Data = f.read()
-
     Program(In_Data, 25, 6)
