4a179b597e69f154c134e2b3269e18f21d0d3300
than5466 <than5466@student.su.se>
2019-12-08 10:44:34 +0100

    Dag 8

---
+++ day_8/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+def Split_To_Layers(Data, layer_width, layer_height):
+    layer_size = layer_width*layer_height
+    layers = []
+    while Data:
+        layer = Data[:layer_size]
+        Data = Data[layer_size:]
+        layers.append(layer)
+    return layers
+
+def Least_Zeroes(Layers):
+    
+    Layer = 0
+    number = float('inf')
+
+    for layer in Layers:
+        if layer.count('0') < number:
+            number = layer.count('0')
+            Layer = layer
+    
+    return Layer
+
+def Program(Data, width, height):
+    Layers = Split_To_Layers(Data,width,height)
+    Fewest_Zeroes = Least_Zeroes(Layers)
+    ans = Fewest_Zeroes.count('1')*Fewest_Zeroes.count('2')
+    print("Answer, Part 1: {}".format(ans))
+
+    image = ['2']*width*height
+    Length = range(len(image))
+    for layer in Layers:
+        for index in Length:
+            if image[index] == '2':
+                if layer[index] == '0':
+                    image[index] = ' '
+                elif layer[index] == '1':
+                    image[index] = '|'
+    
+    image = "".join(image)
+
+    print("Answer, Part 2:")
+    for i in range(height):
+        print("{}".format(image[i*width:(i+1)*width]))
+    
+
+
+
+if __name__ == '__main__':
+    f = open("data.txt", "r")
+    In_Data = f.read()
+
+    Program(In_Data, 25, 6)
