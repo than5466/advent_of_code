742a1da37440556eb6e8be897b86501da1ad4d90
than5466 <than5466@student.su.se>
2019-12-14 20:31:33 +0100

    fortfarande väldigt mycket kvar

--- day_14/main.py
+++ day_14/main.py
@@ -1,23 +1,53 @@
 #!/usr/bin/env python3
 
-def relative_cost(dict):
-    Known_Costs = {'1 ORE':1}
-    Unknown_Costs = set()
-    for k,v in dict.items():
-        for Ore_Number in k.split(", "):
-            Ore = Ore_Number.split(" ")[1]
-            if Ore not in Known_Costs.keys():
-                Unknown_Costs.add(Ore)
-        for Ore_Number in v.split(", "):
-            Ore = Ore_Number.split(" ")[1]
-            if Ore not in Known_Costs.keys():
-                Unknown_Costs.add(Ore)
-    return Known_Costs, Unknown_Costs
+import math
 
-def Program(dict):
-    Known, Unknown = relative_cost(dict)
+def Calculate_Cost(Fuel_Costs, Known_Costs):
+
+    summed_costs = 0
+
+    for each_cost in Fuel_Costs:
+        [Material_Amount,Material_Type] = each_cost.split(' ')
+        Material_Amount = int(Material_Amount)
+        for k,v in Known_Costs.items():
+            if Material_Type in k:
+                divisor = int(k.split(' ')[0])
+                multiplier = int(v.split(' ')[0])
+                summed_costs += math.ceil(Material_Amount/divisor)*multiplier
+                break
+    print(summed_costs)
+
+
+
+def Known_Cost(Data):
+    Known_Costs = {}
+    for k,v in Data.items():
+        if 'ORE' in k:
+            Known_Costs[v] = k
+    return Known_Costs
+
+def Fuel_Cost(Data):
+    Fuel_Costs = {}
+    for k,v in Data.items():
+        if 'FUEL' in v:
+            Fuel_Costs[k] = v
+    return Fuel_Costs
+
+def Recursive_Descent(Data, Known_Costs, Fuel_Cost):
+    pass
+
+
+def Program(Data):
+    Known_Ore_Costs = Known_Cost(Data)
+    Fuel_Costs = Fuel_Cost(Data)
+    Basic_Materials = set()
+    for key in Known_Ore_Costs.keys():
+        Basic_Materials.add(key.split(' ')[1])
+    print(Known_Ore_Costs, Fuel_Costs, Basic_Materials)
+    Calculate_Cost(['7 TLJL'], Known_Ore_Costs)
 
 if __name__ == '__main__':
     f = open("data.txt", "r")
     Data = {x.rstrip().split(" => ")[0]:x.rstrip().split(" => ")[1] for x in f.readlines()}
     #print(Data)
+    Program(Data)
